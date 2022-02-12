0a4a70238d78193dd9ba527781ae12e6a7e5c246
zhangqi <q1.zhang@outlook.com>
2022-02-12 20:29:06 +0800

    feture : vue3

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,9 +1,12 @@
-import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
 
 // register globally
-Vue.component('svg-icon', SvgIcon)
 
+export function registerSvgIconComponent(vueApp) {
+  vueApp.component('SvgIcon', SvgIcon)
+}
+
+// TODO: 这里不是很明白，看起来是想把icon注册到全局，但是好像 requireAll 只是返回了一个数组, 并没有在上面注册
 const req = require.context('./svg', false, /\.svg$/)
 const requireAll = requireContext => requireContext.keys().map(requireContext)
 requireAll(req)
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,4 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-
-Vue.use(Router)
+import { createWebHashHistory, createRouter } from 'vue-router'
 
 /* Layout */
 import Layout from '@/layout'
@@ -387,18 +384,17 @@ export const asyncRoutes = [
   { path: '*', redirect: '/404', hidden: true }
 ]
 
-const createRouter = () => new Router({
+const router = createRouter({
   // mode: 'history', // require service support
+  history: createWebHashHistory(),
   scrollBehavior: () => ({ y: 0 }),
   routes: constantRoutes
 })
 
-const router = createRouter()
-
 // Detail see: https://github.com/vuejs/vue-router/issues/1234#issuecomment-357941465
-export function resetRouter() {
-  const newRouter = createRouter()
-  router.matcher = newRouter.matcher // reset router
-}
+// export function resetRouter() {
+//   const newRouter = createRouter()
+//   router.matcher = newRouter.matcher // reset router
+// }
 
 export default router
